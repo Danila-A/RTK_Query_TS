9e20fd4398ceecef02ad09203c2f7c2056c67f02
Nigger <Nigger@coolboy.com>
2025-06-18 10:51:39 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import { useAddProductMutation, useDeleteProductMutation, useGetGoodsQuery, useUpdateProductMutation } from "./store/goodsApi"
+
+
+export function App() {
+  const [count, setCount] = useState<string>('');
+  
+  const [newProduct, setNewProduct] = useState('');
+
+  const [updatedProduct, setUpdatedProduct] = useState('');
+  const [inputVisible, setInputVisible] = useState(false);
+  const [idProduct, setIdProduct] = useState<number>(0);
+
+  const { data=[], isLoading } = useGetGoodsQuery(count);
+  const [addProduct, {}] = useAddProductMutation();
+  const [deleteProduct, {}] = useDeleteProductMutation();
+  const [updateProduct, {}] = useUpdateProductMutation();
+
+  const handleAddProduct = async () => {
+    if (newProduct) {
+      await addProduct({name: newProduct}).unwrap();
+      setNewProduct('');
+    }
+  }
+
+  const handleDeleteProduct = async (id: number) => {
+    await deleteProduct(id).unwrap();
+  }
+
+  const handleUpdateProduct = async (id: number) => {
+    if(updatedProduct) {
+      await updateProduct({id, name: updatedProduct}).unwrap();
+      setUpdatedProduct('');
+      setInputVisible(false);
+      setIdProduct(0);
+    }
+  }
+  const handleCloseInput = () => {
+    setInputVisible(false);
+    setIdProduct(0);
+  }
+  const handleOpenInput = (id: number) => {
+    setInputVisible(true);
+    setIdProduct(id);
+    setUpdatedProduct(data.filter((item) => item.id === id)[0].name);
+  }
+
+  if (isLoading) return <h1>Loading...</h1>
+
+  return (
+    <div className="">
+
+      <div className="">
+        <input 
+          type="text" 
+          value={newProduct} 
+          onChange={(event) => setNewProduct(event.target.value)} 
+        />
+        <button onClick={handleAddProduct}>Add product</button>
+      </div>
+
+      <div className="">
+        <select value={count} onChange={(event) => setCount(event.target.value)} name="" id="">
+          <option value="">all</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+      </div>
+      
+      {inputVisible && <div>
+        <span className="">
+          <input type="text" onChange={(event) => setUpdatedProduct(event.target.value)} value={updatedProduct} />
+        </span>
+        <button onClick={handleCloseInput}>close</button>
+        <button onClick={() => handleUpdateProduct(idProduct)}>update</button>
+      </div>}
+
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <span className="">
+              {item.name}
+            </span>
+            <button
+              onClick={() => handleDeleteProduct(item.id)}
+            >
+              Del
+            </button>
+            <button onClick={() => handleOpenInput(item.id)}>edit</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
---
+++ src/store/goodsApi.ts
@@ -0,0 +1,44 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { type Product, type ProductList } from '../interfaces';
+
+export const goodsApi = createApi({
+    reducerPath: 'goodsApi',
+    tagTypes: ['Products'],
+    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3001/'}),
+    endpoints: (build) => ({
+        getGoods: build.query<ProductList, string>({
+            query: (limit='') => `goods?${limit && `_limit=${limit}`}`,
+            providesTags: (result) => result 
+                ? [
+                    ...result.map(({id}) => ({ type: 'Products' as const, id })),
+                    { type: 'Products', id: 'LIST' },
+                ]
+                : [{type: 'Products', id: 'LIST'}],
+        }),
+        addProduct: build.mutation<Product, Partial<Product>>({
+            query: (body) => ({
+                url: 'goods',
+                method: 'POST',
+                body,
+            }),
+            invalidatesTags: [{type: 'Products', id: 'LIST'}]
+        }),
+        deleteProduct: build.mutation<Product, number>({
+            query: (id) => ({
+                url: `goods/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: [{type: 'Products', id: 'LIST'}]
+        }),
+        updateProduct: build.mutation<Product, Partial<Product> & Pick<Product, 'id'>>({
+            query: ({id, ...body}) => ({
+                url: `goods/${id}`,
+                method: 'PATCH',
+                body,
+            }),
+            invalidatesTags: [{type: 'Products', id: 'LIST'}]
+        })
+    })
+});
+
+export const { useGetGoodsQuery, useAddProductMutation, useDeleteProductMutation, useUpdateProductMutation } = goodsApi;
